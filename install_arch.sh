556de73200a9f6c3288a39409de58c48dd245171
niknik12345 <34383293+niknik12345@users.noreply.github.com>
2020-01-05 00:50:40 +0200

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -56,7 +56,7 @@ swapon /dev/sda3
 mount /dev/sda4 /mnt/home
 
 echo '3.1 Выбор зеркал для загрузки. Ставим зеркало от Нидерланды'
-echo "Server = http://archlinux.mirror.wearetriple.com/" > /etc/pacman.d/mirrorlist
+echo "Server = http://archlinux.mirror.wearetriple.com/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 
 echo '3.2 Установка основных пакетов'
 pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
