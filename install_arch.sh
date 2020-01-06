79a6fa1510e007152f079541c29fcb8b57da50d7
niknik12345 <34383293+niknik12345@users.noreply.github.com>
2020-01-06 13:04:19 +0200

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -1,68 +1,43 @@
 #!/bin/bash
 
+# Arch Linux Install - Быстрая установка Arch Linux
+
 loadkeys ru
 setfont cyr-sun16
 echo 'Скрипт по Установке ArchLinux'
-echo 'Ссылка'
+
 echo '2.3 Синхронизация системных часов'
 timedatectl set-ntp true
 
-echo '2.4 создание разделов'
+echo 'Создайте 3-и раздела'
+echo '1-й efi 200-500M'
+echo '2-й swap 4-8G'
+echo '3-й root для Linux'
 
 cfdisk
-#(
-#  echo o;
-
-#  echo n;
-#  echo;
-#  echo;
-#  echo;
-#  echo +100M;
-
-#  echo n;
-#  echo;
-#  echo;
-#  echo;
-#  echo +4G;
-
-#  echo n;
-#  echo;
-#  echo;
-#  echo;
-#  echo +2G;
-
-#  echo n;
-#  echo p;
-#  echo;
-#  echo;
-#  echo a;
-#  echo 1;
-
-#  echo w;
-#) | fdisk /dev/sda
 
 echo 'Ваша разметка диска'
 fdisk -l
 
 echo '2.4.2 Форматирование дисков'
-mkfs.ext2  /dev/sda1 -L boot
-mkfs.ext4  /dev/sda4 -L root
-mkswap /dev/sda3 -L swap
+
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4  /dev/sda3
 
 echo '2.4.3 Монтирование дисков'
-mount /dev/sda4 /mnt
-mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
-swapon /dev/sda3
+mount /dev/sda3 /mnt
+mkdir -p /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
 
-
-echo '3.1 Выбор зеркал для загрузки. Ставим зеркало от Нидерланды'
-echo "Server = http://ftp.nluug.nl/os/Linux/distr/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+echo '3.1 Выбор зеркал для загрузки.'
+rm -rf /etc/pacman.d/mirrorlist
+wget https://git.io/mirrorlist
+mv -f ~/mirrorlist /etc/pacman.d/mirrorlist
 
 echo '3.2 Установка основных пакетов'
-pacstrap /mnt base base-devel linux linux-firmware linux-headers nano dhcpcd netctl
+pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
 
 echo '3.3 Настройка системы'
-genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -pU /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt sh -c "$(curl -fsSL git.io/install_arch2.sh)"
