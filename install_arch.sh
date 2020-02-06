9571a7d01409f0c5071cbcdac3ef1a7bd53cf3da
niknik12345 <34383293+niknik12345@users.noreply.github.com>
2020-02-06 19:01:27 +0200

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -24,31 +24,7 @@ timedatectl set-ntp true
 #mkfs.fat -F32 /dev/sda1
 #mkswap /dev/sda1 -L swap
 #mkfs.ext4  /dev/sda2
-echo '2.4 создание разделов'
-(
-  echo o;
-
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo +100M;
-
-  echo n;
-  echo;
-  echo;
-  echo;
-  echo +10G;
-
-  echo n;
-  echo p;
-  echo;
-  echo;
-  echo a;
-  echo 1;
-
-  echo w;
-) | fdisk /dev/sda
+echo 'создайте 3 раздела 1-boot/2-root/3-swap'
 
 echo '2.4.2 Форматирование дисков'
 mkfs.ext2  /dev/sda1 -L boot
@@ -57,7 +33,7 @@ mkswap /dev/sda3 -L swap
 
 echo '2.4.3 Монтирование дисков'
 mount /dev/sda2 /mnt
-mkdir /mnt/{boot,home}
+mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 swapon /dev/sda3
 
